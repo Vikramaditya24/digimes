18e0720f69c72fc23d13e2c952887090c12a50d6
Vikramaditya24 <81467681+Vikramaditya24@users.noreply.github.com>
2022-07-12 21:28:57 +0530

    Update settings.py

--- DigiMES/settings.py
+++ DigiMES/settings.py
@@ -137,5 +137,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anantvikramadityar.s1914074@gmail.com'
-EMAIL_HOST_PASSWORD = 'SHRIsharada@1'
